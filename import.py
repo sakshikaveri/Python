4b8bc8ddae2db7222410e8817309f49d218c631e
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2023-11-27 19:29:43 +0530

    import in python

---
+++ import.py
@@ -0,0 +1,13 @@
+# import methods
+import math
+print(math.sqrt(25))
+print(math.sqrt(7))
+print(math.floor(2.9))
+print(math.ceil(2.1))
+print(3**2)
+print(math.pow(3,2))
+
+from math import pow
+print(math.sqrt(9))
+print(math.pow(4,5))
+help('math')
