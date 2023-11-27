c00ea8820b716acec602eec31c72fdab05c6a892
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2023-11-27 16:14:40 +0530

    Bitwise operators

---
+++ Bitwise_operators.py
@@ -0,0 +1,23 @@
+# Complement
+print(~12)
+'''Explanation:
+1) one's complement of 12= 00001100=11110011
+2) 2's complement of -13-- we always store psotive values in system to store negative
+values we have to do 2's complement
+3) 2's complement of 13- one's complement+1= 11110011'''
+print(~45)
+
+# bitwise and
+print(47&21)
+
+# bitwise or
+print(47|21)
+
+# xor- 0 for even number of ones and 1 for odd number of ones
+# bitwise xor
+print(12^20)
+
+# bitwise left shift operators- we gain bits
+print(10<<2)
+# bitwise right shift operators - we loose bits
+print(10>>2)
