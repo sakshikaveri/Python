374a81254339d8c733fc16d6c82f8039af58e2e2
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2023-11-27 10:54:44 +0530

    Number_system_conversion

---
+++ Number_system_conversion.py
@@ -0,0 +1,18 @@
+# from decimal to binary and vice versa
+# 25- 11001
+binary=bin(25)
+print(binary)
+decimal=0b11001
+print(decimal)
+
+#from decimal to octal, decimal to hexadecimal and vice versa
+octal=oct(25)
+print(octal)
+decimal=0o31
+print(decimal)
+hexadecimal=hex(10)
+print(hexadecimal)
+decimal=0xa
+print(decimal)
+
+
